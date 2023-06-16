696ae1432a616da9f7d0390cebc233762bbf424c
Götz Elisabeth <eg054@hdm-stuttgart.de>
2023-06-16 14:17:25 +0200

    Scanner importiert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,9 @@
 package org.example;
 import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        final Scanner scan = new Scanner(System.in);
+        final int userinput = scan.nextInt();
+                System.out.print(userinput);
+    }
 }
