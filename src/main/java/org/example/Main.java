19bd7f6e10ff0dd5a011f96496758468bbb12575
Götz Elisabeth <eg054@hdm-stuttgart.de>
2023-06-16 14:19:33 +0200

    Scanner importiert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a currency:");
         final int userinput = scan.nextInt();
                 System.out.print(userinput);
     }
