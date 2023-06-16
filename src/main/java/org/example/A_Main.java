98d7336ed497d4c6aa926958e03897c6184b230f
malenabockmann <mb429@hdm-stuttgart.de>
2023-06-16 16:42:26 +0200

    File reader geschrieben

--- src/main/java/org/example/A_Main.java
+++ src/main/java/org/example/A_Main.java
@@ -1,13 +1,15 @@
 package org.example;
 import java.util.Scanner;
+import java.io.*;
 public class A_Main {
 
     /**
      * import java.util.Scanner
      * Definition sout
+     *
      * @param args
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         final Scanner scan = new Scanner(System.in);
         System.out.println("Currency to buy: ");
         System.out.println("Currency to sell: ");
@@ -16,44 +18,57 @@ public class A_Main {
         System.out.println("1: Select currency to sell");
         System.out.println("2: Chose amount to be converted:");
         System.out.println("Please chose an option (<<x>> to exit): ");
-
+           */
 
         /**
          * @param userInput
          */
-
+         /*
         final String userInput = scan.next();
-                System.out.print(userInput);
+        System.out.print(userInput);
         final C_Interface clear = new C_Interface();
 
-                switch (userInput) {
-                    case "0":
-                        System.out.println("Enter a currency's name or part of it (>>x<< to exit): ");
-                        break;
-                    case "1":
-                        System.out.println("Enter a currency's name or part of it (>>x<< to exit): ");
-                        break;
-                    case "2":
-                        System.out.println("Enter an amount: ");
-                        final String AMOUNT = scan.next();
-                        break;
-                    case "x":
-                         clear.clearConsole();
-                         break; 
-                    default:
-                        System.out.println("bye");;
-                }
+        switch (userInput) {
+            case "0":
+                System.out.println("Enter a currency's name or part of it (>>x<< to exit): ");
+                break;
+            case "1":
+                System.out.println("Enter a currency's name or part of it (>>x<< to exit): ");
+                break;
+            case "2":
+                System.out.println("Enter an amount: ");
+                final String AMOUNT = scan.next();
+                break;
+            case "x":
+                clear.clearConsole();
+                break;
+            default:
+                System.out.println("bye");
+                ;
+        }
 
 
+    }
+        */
 
-
-
-
+    public static void main(String[] args) {
+        try {
+            File getCSVFiles = new File("./Projekt SE1/rms_mth3.csv");
+            Scanner sc = new Scanner(getCSVFiles);
+            sc.useDelimiter(";");
+            while (sc.hasNext()) {
+                System.out.print(sc.next() + "|");
+            }
+            sc.close();
+        } catch(IOException ioe) {
+            System.out.println("This file cannot be reached");
+        }
 
 
 
 
 
     }
-
 }
+
+
