07dde199fb0946a7c7d81a68883bb032b9a9760b
malenabockmann <mb429@hdm-stuttgart.de>
2023-06-16 14:32:21 +0200

    Start Template

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a currency:");
-        final int userinput = scan.nextInt();
-                System.out.print(userinput);
+        System.out.println("Currency to buy: ");
+        System.out.println("Currency to sell: ");
+        System.out.println("++++++++++++++++++++++");
+        System.out.println("0: Select currency to buy");
+        System.out.println("1: Select currency to sell");
+        System.out.println("2: Chose amount to be converted:");
+        System.out.println("Please chose an option (<<x>> to exit): ");
+        final String userInput = scan.next();
+                System.out.print(userInput);
+
     }
 }
